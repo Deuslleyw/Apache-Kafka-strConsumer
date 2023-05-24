380ad5c882bf43467b3cbdb55185938b4ce48645
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2023-05-24 00:54:03 -0300

    groupH

--- src/main/java/com/deusley/strconsumer/listeners/StrConsumerListener.java
+++ src/main/java/com/deusley/strconsumer/listeners/StrConsumerListener.java
@@ -20,4 +20,9 @@ public class StrConsumerListener {
     public void log(String message) {
         log.info("LOG ::: Receive message {}", message);
     }
+
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void history(String message){
+        log.info("HISTORY ::: Receive message {}", message);
+    }
 }
